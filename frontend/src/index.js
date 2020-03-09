77123f0333219a6857c37967395d1af78b1641e4
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-09 08:41:53 -0400

    Select elements and set up fetch

---
+++ frontend/src/index.js
@@ -0,0 +1,34 @@
+// global
+const parseJSON = response => response.json()
+const addBtn = document.getElementById('new-project-btn')
+const projectForm = document.querySelector('.container')
+const projectCollection = document.querySelector("#project-collection")
+const familyProjectsBelongTo = document.getElementById('family-project-list')
+const familyPopUp = document.getElementById('family-pop-up')
+const addFamilyBtn = document.getElementById('add-new-family')
+const selectFamilyBtn = document.getElementById('all-family-options')
+const familySelectionPopUp = document.querySelector('.family-selector')
+const selectFamily = document.getElementById('family-select') 
+const selectForm = document.querySelector('.family-selector')
+const select = document.querySelector("#select")
+let addChore = false 
+let addHouseHold = false
+let selectHouse = false
+
+// api
+class Api {
+    static baseUrl = 'http://localhost:3000'
+    static PROJECTS_URL = `${Api.baseUrl}/projects`
+    static FAMILIES_URL = `${Api.baseUrl}/families`
+
+    static fetchProjects() {
+        return fetch(Api.baseUrl + '/projects')
+            .then(parseJSON)
+    }
+    
+    static fetchFamilies(){
+        return fetch(Api.baseUrl + '/families')
+            .then(parseJSON)
+    }
+}
+
