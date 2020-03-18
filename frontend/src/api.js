43a2d27681c7312e55f1e608a3507303ca97b706
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-17 20:39:33 -0400

    Add notes to api class

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,35 +1,31 @@
-// displays message in console when this file is loaded
+// Displays message in console when this file is loaded
 document.addEventListener('DOMContentLoaded', 
 () => {
     console.log('api.js loaded')
 });
 
-// Example of Object Oriented JavaScript
-// class Api to encapsulate all information/behaviors that represents the apis
-// this Api class holds our functions for fetching our project and family apis
+// Object Oriented JavaScript
+// Class Api to encapsulate all information/behaviors that represents the apis
+// This Api class holds our functions for fetching our project and family apis
 class Api {
     static baseUrl = 'http://localhost:3000'
     static PROJECTS_URL = `${Api.baseUrl}/projects`
     static FAMILIES_URL = `${Api.baseUrl}/families`
 
+    // Fetch request is our GET request for our projects
+    // Fetch returns a Promise: handles asynchronous operations without the need for a callback.
+    // To do something after the resource is fetched, you write it in a .then call
+    // What we requested is hidden in body as a readable stream. We need to call an appropriate method to convert this readable stream into data we can consume.
+    // We know the response is JSON. We can call our function (parseJSON) (which is response.json) to convert the data.
+    // This was defined in index.js: const parseJSON = response => response.json()
     static fetchProjects() {
         return fetch(Api.baseUrl + '/projects')
             .then(parseJSON)
     }
     
+    // Fetch request is our GET request for our families
     static fetchFamilies(){
         return fetch(Api.baseUrl + '/families')
             .then(parseJSON)
     }
 }
-
-// fetch request
-
-// These fetch requests are our GET requests
-// Fetch returns a Promise: handles asynchronous operations without the need for a callback.
-// To do something after the resource is fetched, you write it in a .then call:
-// What we requested is hidden in body as a readable stream. We need to call an appropriate method to convert this readable stream into data we can consume.
-// We know the response is JSON. We can call our function (parseJSON) (which is response.json) to convert the data.
-// This was defined in index.js: const parseJSON = response => response.json()
-
-// Need to write this out w/o arrow function for practice
